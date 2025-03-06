e88466fd8b65314b6ab5942d71d3b24f9f85ea49
RamcharanVallapureddy <charan.9sd@gmail.com>
2025-03-06 23:42:42 +0530

    Added <file-name> to repository

---
+++ index.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+console.log(inputBox.value);
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Task cannot be Empty");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value='';
+    saveData();
+}
+listContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function history(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+history();
